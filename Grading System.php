fa94940735209d07e2c6201ed714741dbcce0721
Christian Encinares Bocz <144127538+xtianencinares@users.noreply.github.com>
2024-03-06 14:09:21 +0800

    Update Grading System.php

--- Grading System.php
+++ Grading System.php
@@ -25,7 +25,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.    
  * 
- * @package	CodeIgniter
+ * @package	CodeIgniter 
  * @author	EllisLab Dev Team
  * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (http://ellislab.com/) 
  * @copyright	Copyright (c) 2014 - 2015, British Columbia Institute of Technology (http://bcit.ca/)
