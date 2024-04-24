72b320431d8092ef75577b7824fea28e65e203a6
Christian Encinares Bocz <144127538+xtianencinares@users.noreply.github.com>
2024-04-24 12:35:49 +0800

    Update Grading System.php

--- Grading System.php
+++ Grading System.php
@@ -209,7 +209,7 @@ switch (ENVIRONMENT)
 
 /*
  * -------------------------------------------------------------------
- *  Now that we know the path, set the main path constants
+ *  Now that we know the path, set the main path constants.
  * -------------------------------------------------------------------
  */
 	// The name of THIS file
