aeecc8a03cc35b48778416096d269a12849bc15d
Christian Encinares Bocz <144127538+xtianencinares@users.noreply.github.com>
2024-08-21 16:27:49 +0800

    Update index.php

--- index.php
+++ index.php
@@ -56,9 +56,9 @@
 	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
 
 /*
- *---------------------------------------------------------------
+ *----------------------------------------------------------------
  * ERROR REPORTING
- *---------------------------------------------------------------
+ *----------------------------------------------------------------
  *
  * Different environments will require different levels of error reporting.
  * By default development will show errors but testing and live will hide them.
