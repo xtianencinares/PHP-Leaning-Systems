15c5fcb15e9c65f361844a3b6a55c6bf52216779
Christian Encinares Bocz <144127538+xtianencinares@users.noreply.github.com>
2025-05-21 14:17:32 +0800

    Update Grading System.php

--- Grading System.php
+++ Grading System.php
@@ -54,7 +54,7 @@
 /*
  *------------------------------------------------------------
  * ERROR REPORTING v1.20.1
- *-------------------------------------------------------------
+ *------------------------------------------------------------
  *
  * Different environments will require different levels of error reporting
  * By default development will show errors but testing and live will hide them.
